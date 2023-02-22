6a11165aa08640ad75402e034f962f8a82f6337b
faridrma <faridrma@code.edu.az>
2023-02-22 13:30:53 +0400

    fourth

---
+++ AllupTask/Interfaces/ILayoutService.cs
@@ -0,0 +1,13 @@
+﻿using AllupTask.Models;
+using AllupTask.ViewModels.BasketViewModels;
+
+namespace AllupTask.Interfaces
+{
+    public interface ILayoutService
+    {
+        Task<IDictionary<string,string>> GetSettings();
+        Task<IEnumerable<Category>> GetCategories();
+
+        Task<List<BasketVM>> GetBaskets();
+    }
+}
